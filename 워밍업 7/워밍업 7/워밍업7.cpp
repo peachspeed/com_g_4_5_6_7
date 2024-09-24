b807098efb0bf47c7519e2117ca9feb9dd157460
peachspeed_com <131349605+peachspeed@users.noreply.github.com>
2024-09-24 09:27:37 +0900

    컴그 찐 재출

---
+++ 워밍업 7/워밍업 7/워밍업7.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+#define SIZE 30
+
+// 보드를 초기화하는 함수
+void Board(char board[][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            board[i][j] = '.';
+        }
+    }
+}
+
+// 보드를 출력하는 함수
+void printBoard(char board[][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            cout << setw(2) << board[i][j];
+        }
+        cout << '\n';
+    }
+}
+
+// 사각형을 그리는 함수 (좌우로 넘어갈 때 wrap-around 처리)
+void draw(char board[][SIZE], int x1, int y1, int x2, int y2) {
+    for (int i = y1; i <= y2; i++) {
+        for (int j = x1; j <= x2; j++) {
+            int wrapX = (j + SIZE) % SIZE; // x축을 보드 크기로 나눠서 wrap-around
+            int wrapY = (i + SIZE) % SIZE; // y축도 마찬가지로 처리
+            board[wrapY][wrapX] = '#'; // 사각형을 '#'로 그린다
+        }
+    }
+}
+
+// 사각형을 지우는 함수 (wrap-around 적용)
+void clear(char board[][SIZE], int x1, int y1, int x2, int y2) {
+    for (int i = y1; i <= y2; i++) {
+        for (int j = x1; j <= x2; j++) {
+            int wrapX = (j + SIZE) % SIZE;
+            int wrapY = (i + SIZE) % SIZE;
+            board[wrapY][wrapX] = '.'; // 사각형을 지운다
+        }
+    }
+}
+
+int main() {
+    char board[SIZE][SIZE];
+    int x1, y1, x2, y2;
+    char command;
+
+    Board(board);
+
+    cout << "사각형의 좌표 (x1, y1) (x2, y2)를 입력하세요: ";
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    // 보드에 사각형 그리기
+    draw(board, x1, y1, x2, y2);
+    printBoard(board);
+
+    while (true) {
+        cout << "명령어 (x/X, y/Y, s/S, i/I, j/J, m, r, q): ";
+        cin >> command;
+
+        // 현재 사각형을 지움
+        clear(board, x1, y1, x2, y2);
+
+        switch (command) {
+        case 'x':  // x 축을 오른쪽으로 이동
+            x1 = (x1 + 1) % SIZE;
+            x2 = (x2 + 1) % SIZE;
+            break;
+        case 'X':  // x 축을 왼쪽으로 이동
+            x1 = (x1 - 1 + SIZE) % SIZE;
+            x2 = (x2 - 1 + SIZE) % SIZE;
+            break;
+        case 'y':  // y 축을 아래로 이동
+            y1 = (y1 + 1) % SIZE;
+            y2 = (y2 + 1) % SIZE;
+            break;
+        case 'Y':  // y 축을 위로 이동
+            y1 = (y1 - 1 + SIZE) % SIZE;
+            y2 = (y2 - 1 + SIZE) % SIZE;
+            break;
+        case 's':  // 사각형 축소
+            if (x1 < x2 && y1 < y2) {
+                x2 = (x2 > 0) ? x2 - 1 : x2;
+                y2 = (y2 > 0) ? y2 - 1 : y2;
+            }
+            break;
+        case 'S':  // 사각형 확대
+            x2 = (x2 < SIZE - 1) ? x2 + 1 : x2;
+            y2 = (y2 < SIZE - 1) ? y2 + 1 : y2;
+            break;`
+        case 'm':  // 현재 사각형의 좌표를 출력
+            cout << "사각형 좌표: (" << x1 << ", " << y1 << "), (" << x2 << ", " << y2 << ")\n";
+            break;
+        case 'r':  // 새로운 좌표를 입력받아 사각형을 다시 그림
+            cout << "새 좌표 (x1, y1) (x2, y2)를 입력하세요: ";
+            cin >> x1 >> y1 >> x2 >> y2;
+            break;
+        case 'q':  // 프로그램 종료
+            cout << "프로그램을 종료합니다.\n";
+            return 0;
+        default:
+            cout << "유효하지 않은 명령어입니다.\n";
+            continue;
+        }
+
+        // 새로운 위치에 사각형을 그리기
+        draw(board, x1, y1, x2, y2);
+        printBoard(board);
+    }
+
+    return 0;
+}
---
+++ 컴그 워밍업6/컴그 워밍업6/컴그 워밍업 6.cpp
@@ -0,0 +1,196 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
+#include <conio.h>
+
+using namespace std;
+
+#define SIZE 30
+#define MAX_OBSTACLES 30
+#define MAX_STEPS_IN_ONE_DIRECTION 5
+
+char board[SIZE][SIZE];
+int path[SIZE][SIZE]; // 경로 표시용
+int playerX, playerY; // 플레이어 위치
+
+// 콘솔 색상을 설정하는 함수
+void setColor(int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void initializeBoard() {
+    // 보드를 배경(1)으로 초기화
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            board[i][j] = '1';
+            path[i][j] = 1; // 경로는 배경으로 초기화
+        }
+    }
+}
+
+// 장애물을 경로 근처에 배치
+void placeObstacles() {
+    int placedObstacles = 0;
+    while (placedObstacles < MAX_OBSTACLES) {
+        int x = rand() % SIZE;
+        int y = rand() % SIZE;
+
+        // 장애물은 경로 바로 옆에만 배치되도록
+        if (path[y][x] == 0) continue; // 경로 위에는 장애물 배치하지 않음
+
+        bool nearPath = false;
+        // 경로 근처(좌/우/상/하에 경로가 있으면)에만 장애물 배치
+        if (x > 0 && path[y][x - 1] == 0) nearPath = true;
+        if (x < SIZE - 1 && path[y][x + 1] == 0) nearPath = true;
+        if (y > 0 && path[y - 1][x] == 0) nearPath = true;
+        if (y < SIZE - 1 && path[y + 1][x] == 0) nearPath = true;
+
+        if (nearPath) {
+            board[y][x] = 'X';
+            placedObstacles++;
+        }
+    }
+}
+
+// 랜덤 방향을 선택하는 함수 (0: 우, 1: 하, 2: 좌, 3: 상)
+int randomDirection(int currentDirection) {
+    int newDirection = rand() % 4;
+
+    // 같은 방향으로 계속 가지 않게 함
+    while ((currentDirection == 0 && newDirection == 2) ||
+        (currentDirection == 1 && newDirection == 3) ||
+        (currentDirection == 2 && newDirection == 0) ||
+        (currentDirection == 3 && newDirection == 1)) {
+        newDirection = rand() % 4;
+    }
+
+    return newDirection;
+}
+
+// 좌측 상단에서 우측 하단까지 경로를 한 줄로 생성
+void generatePath() {
+    int x = 0, y = 0;
+    int direction = rand() % 4; // 0: 우, 1: 하, 2: 좌, 3: 상
+    int stepCount = 0; // 현재 방향으로 이동한 칸 수
+
+    board[y][x] = '0';
+    path[y][x] = 0;
+
+    while (x != SIZE - 1 || y != SIZE - 1) {
+        if (stepCount >= rand() % MAX_STEPS_IN_ONE_DIRECTION + 1) {
+            // 무작위로 1~5칸 사이의 값으로 이동 후 방향 전환
+            direction = randomDirection(direction);
+            stepCount = 0;
+        }
+
+        if (direction == 0 && x < SIZE - 1) {
+            // 오른쪽으로 이동
+            x++;
+        }
+        else if (direction == 1 && y < SIZE - 1) {
+            // 아래로 이동
+            y++;
+        }
+        else if (direction == 2 && x > 0) {
+            // 왼쪽으로 이동
+            x--;
+        }
+        else if (direction == 3 && y > 0) {
+            // 위로 이동
+            y--;
+        }
+        else {
+            // 만약 이동 불가능하면 방향 전환
+            direction = randomDirection(direction);
+            stepCount = 0;
+            continue; // 바로 다음 이동으로 넘어감
+        }
+
+        board[y][x] = '0';
+        path[y][x] = 0;
+        stepCount++;
+    }
+}
+
+void printBoard() {
+    system("CLS");
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (i == playerY && j == playerX) {
+                setColor(4); // 빨간색으로 플레이어 위치 출력
+                cout << "* ";
+                setColor(7); // 기본 색상으로 복귀
+            }
+            else if (board[i][j] == 'X') {
+                setColor(1); // 파란색으로 장애물 출력
+                cout << "X ";
+                setColor(7); // 기본 색상으로 복귀
+            }
+            else {
+                cout << board[i][j] << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void movePlayer(char command) {
+    int newX = playerX, newY = playerY;
+
+    switch (command) {
+    case 'w': newY--; break; // 상
+    case 's': newY++; break; // 하
+    case 'a': newX--; break; // 좌
+    case 'd': newX++; break; // 우
+    }
+
+    // 경로 위에서만 이동 가능
+    if (newX >= 0 && newX < SIZE && newY >= 0 && newY < SIZE && path[newY][newX] == 0) {
+        playerX = newX;
+        playerY = newY;
+    }
+}
+
+int main() {
+    srand(time(0));
+
+    initializeBoard();
+    generatePath();
+    placeObstacles();
+
+    // 초기 플레이어 위치 설정 (좌측 상단)
+    playerX = 0;
+    playerY = 0;
+
+    char command;
+    bool isPlaying = true;
+
+    while (isPlaying) {
+        printBoard();
+
+        command = _getch();
+
+        switch (command) {
+        case 'r': // 객체 생성 (이미 좌측 상단에 있음)
+            playerX = 0;
+            playerY = 0;
+            break;
+        case 'w': case 'a': case 's': case 'd': // 이동
+            movePlayer(command);
+            break;
+        case '\r': // Enter: 새로운 경로 생성
+            initializeBoard();
+            generatePath();
+            placeObstacles();
+            playerX = 0;
+            playerY = 0;
+            break;
+        case 'q': // 종료
+            isPlaying = false;
+            break;
+        }
+    }
+
+    return 0;
+}
